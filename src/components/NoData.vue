a92a44fd9befb2e4b5d0dfb0a42d44ff4e5624f5
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-05-16 22:06:41 +0200

    add no data component

---
+++ src/components/NoData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :class="style.container"
+    class="d-flex justify-center align-center w-50 h-100"
+  >
+    <h2 :class="style.text">
+      NO DATA
+    </h2>
+  </div>
+</template>
+ 
+<style lang="scss" module="style">
+@use '../scss/colors';
+@use '../scss/fonts';
+@use '../scss/tokens';
+  
+.container {
+  background: colors.$blue-gradient;
+  border-radius: 5px;
+}
+
+.text {
+  font-family: fonts.$main-font;
+  font-size: tokens.$font-size-default;
+  color: colors.$white;
+}
+</style>
+  
